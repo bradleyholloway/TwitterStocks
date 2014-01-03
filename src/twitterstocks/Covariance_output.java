095bb72dc6a6fb53b87f434eaa9bc3420ac69822
dailerob <dailey.roberto@gmail.com>
2014-01-03 11:34:16 -0600

    practice

--- src/twitterstocks/Covariance_output.java
+++ src/twitterstocks/Covariance_output.java
@@ -4,16 +4,33 @@
  */
 package twitterstocks;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author roberto
  */
+
+
 public class Covariance_output {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            Database.load();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Covariance_output.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ArrayList<String> words = Database.words;
+        for(String word : words)
+        {
+            System.out.println(Compare.covariance(Database.getIndicatorGraph("gdp by quarter"), Database.getCountOfWordGraph(word)));
+        }
     }
 }
