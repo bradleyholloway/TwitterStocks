aade07fef5c33ddaa18046f076b503a33ea85fb5
Bradley Holloway <drummerboy400@gmail.com>
2014-02-06 14:25:42 -0600

    system out print...

--- src/twitterstocks/VectorAnalysis.java
+++ src/twitterstocks/VectorAnalysis.java
@@ -54,7 +54,7 @@ public class VectorAnalysis {
                 bestWord = word;
             }
         }
-        
+        System.out.println(bestWord + " : " + minimumDistance);
         
     }
 }
