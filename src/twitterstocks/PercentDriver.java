fe13cd779218f4a7004cb63e9a99596cda26db61
Bradley Holloway <drummerboy400@gmail.com>
2014-02-11 14:19:27 -0600

    reformat

--- src/twitterstocks/PercentDriver.java
+++ src/twitterstocks/PercentDriver.java
@@ -20,45 +20,44 @@ public class PercentDriver {
         Database.loadIndicators();
         int iterations = 50;
 
-        for (Indicator indicator : Database.indicators)
-        {
-        //Example of How to lod in Data using the GSON loaders (REDO WHEN INDICATOR CHANGES)
-        HashMap<String, float[]> percentZVectors = Database.getGSONMap(indicator.getName());
-        float[] indicatorPerData = Database.getGSONIndicator(indicator.getName());
-        //double[][] indicatorData = Database.getIndicatorGraph(indicator);
-        //float[] indicatorPerData = Database.getIndicatorVector(indicatorData);
-        
-        //Example of How to load in the Percent GSON data
-        
-        //HashMap<String, float[]> percentZVectors = Database.getGSONPerMap(indicator.getName());
-        //float[] indicatorPerData = Database.getGSONIndicatorPer(indicator.getName());
-        float[] goal = copy(indicatorPerData);
-        float[] finalGoal = copy(goal);
-        float[] total = new float[goal.length];
-        String bestWord = "";
-        double tempDistance;
-        
-        for (int iteration = 0; iteration < iterations; iteration++) {
-            double minimumDistance = Double.MAX_VALUE;
-            //System.out.println("Searching for closest vector... Iteration: " + iteration);
-            for (String word : Database.RevWords) {
-                if (percentZVectors.get(word) != null) {
-                    tempDistance = Compare.distanceBetweenScaled(goal, percentZVectors.get(word));
-                    if (tempDistance < minimumDistance) {
-                        minimumDistance = tempDistance;
-                        bestWord = word;
-                        //System.out.println("New Best: " + bestWord);
+        for (Indicator indicator : Database.indicators) {
+            //Example of How to lod in Data using the GSON loaders (REDO WHEN INDICATOR CHANGES)
+            HashMap<String, float[]> percentZVectors = Database.getGSONMap(indicator.getName());
+            float[] indicatorPerData = Database.getGSONIndicator(indicator.getName());
+            //double[][] indicatorData = Database.getIndicatorGraph(indicator);
+            //float[] indicatorPerData = Database.getIndicatorVector(indicatorData);
+
+            //Example of How to load in the Percent GSON data
+
+            //HashMap<String, float[]> percentZVectors = Database.getGSONPerMap(indicator.getName());
+            //float[] indicatorPerData = Database.getGSONIndicatorPer(indicator.getName());
+            float[] goal = copy(indicatorPerData);
+            float[] finalGoal = copy(goal);
+            float[] total = new float[goal.length];
+            String bestWord = "";
+            double tempDistance;
+
+            for (int iteration = 0; iteration < iterations; iteration++) {
+                double minimumDistance = Double.MAX_VALUE;
+                //System.out.println("Searching for closest vector... Iteration: " + iteration);
+                for (String word : Database.RevWords) {
+                    if (percentZVectors.get(word) != null) {
+                        tempDistance = Compare.distanceBetweenScaled(goal, percentZVectors.get(word));
+                        if (tempDistance < minimumDistance) {
+                            minimumDistance = tempDistance;
+                            bestWord = word;
+                            //System.out.println("New Best: " + bestWord);
+                        }
                     }
-                }
-            } //System.out.println("Done.");
-            //System.out.println(bestWord + ", Weighted at: " + Compare.correctScale(goal, wordVectors.get(bestWord)));
-            total = Compare.add(total, Compare.multiply(percentZVectors.get(bestWord), Compare.correctScale(goal, percentZVectors.get(bestWord))));
-            goal = Compare.getDifference(finalGoal, total);
-            //Grapher.createGraph(total, finalGoal, "Vector" + bestWord + "CTG" + iteration);
-        }
-        Grapher.createGraph(total, finalGoal, "Vector" + indicator.getName() + "Approximation");
-        //Grapher.createGraph(total, "Vector" + indicator.getName() + "Predicted");
-        //Grapher.createGraph(finalGoal, "Vector" + indicator.getName() + "Goal");
+                } //System.out.println("Done.");
+                //System.out.println(bestWord + ", Weighted at: " + Compare.correctScale(goal, wordVectors.get(bestWord)));
+                total = Compare.add(total, Compare.multiply(percentZVectors.get(bestWord), Compare.correctScale(goal, percentZVectors.get(bestWord))));
+                goal = Compare.getDifference(finalGoal, total);
+                //Grapher.createGraph(total, finalGoal, "Vector" + bestWord + "CTG" + iteration);
+            }
+            Grapher.createGraph(total, finalGoal, "Vector" + indicator.getName() + "Approximation");
+            //Grapher.createGraph(total, "Vector" + indicator.getName() + "Predicted");
+            //Grapher.createGraph(finalGoal, "Vector" + indicator.getName() + "Goal");
         }
         /*double[][] indicatorGraph = Database.getIndicatorGraph(Database.indicators.get(2));
          double[][] wordPercent = Database.getPercentOfWordGraph(" the ");
@@ -74,8 +73,8 @@ public class PercentDriver {
          Grapher.createGraph(vectorI, "Test PercentsI");
          System.out.println("Done.");*/
     }
-    public static float[] copy(float[] data)
-    {
+
+    public static float[] copy(float[] data) {
         float[] returns = new float[data.length];
         System.arraycopy(data, 0, returns, 0, data.length);
         return returns;
