a99edb98d3edfe408a27f3cf8ef7c183481fbd62
dailerob <dailey.roberto@gmail.com>
2014-02-16 22:09:32 -0600

    yahoo commit

---
+++ src/twitterstocks/yahooFinanceMiner.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitterstocks;
+
+/**
+ *
+ * @author roberto
+ */
+public class yahooFinanceMiner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WebRobot robot = new WebRobot();
+        Database.load();
+        robot.launchChrome();
+
+        robot.mineYahooFinance();
+    }
+}
